86753e93c8385431dfbfb6d5c102ccc8f64baf10
João Pedro Almeida Tenório <jupiramanteiga@gmail.com>
2022-05-16 22:02:50 -0300

    Estilização

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
+const btnoff = document.querySelector('.btnoff')
+const btnon = document.querySelector('.btnon')
 
-let day = getComputedStyle(document.documentElement).getPropertyValue('--hue'); 
-let night = getComputedStyle(document.documentElement).getPropertyValue('--night'); 
+function changecolor() {
+    let hue = getComputedStyle(document.documentElement).getPropertyValue('--hue'); 
+    setInterval(() => {
+        hue++;
+        document.body.style.setProperty('--hue', `${hue}`);
+    }, 100);
+}
 
 function nightmodeon(){
     document.body.style.setProperty('--secondary', 'hsl(var(--hue), 50%, 15%)');
+    btnoff.style.display = 'none';
+    btnon.style.display = 'initial';
 }
 function nightmodeoff(){
     document.body.style.setProperty('--secondary', 'hsl(var(--hue), 60%, 98%)');
+    btnon.style.display = 'none';
+    btnoff.style.display = 'initial';
+    
 }
+changecolor();
